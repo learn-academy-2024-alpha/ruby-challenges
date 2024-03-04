2e10d7d6f215f99a101ba378a451ce10185deed2
Rlemus93 <rlemus93@gmail.com>
2024-03-04 14:57:34 -0800

    next is seth

---
+++ conditionals-methods-ryan-seth.rb
@@ -0,0 +1,67 @@
+# ❤️ Challenges
+# Create a method called sum_these_numbers which takes two integers as an argument and prints their sum to the screen.
+# def sum_these_numbers(num1, num2)
+#     num1 + num2
+# end
+# p sum_these_numbers(5, 8)
+
+# Create a method called is_even, which takes a single integer, and which then returns true if the number is even, and false otherwise.
+# def is_even(num)
+#   if num % 2 == 0
+#     "#{num} is even"
+#   else 
+#     "#{num} is odd"
+#   end
+# end
+
+# p is_even(2)
+# p is_even(3)
+
+
+# Create a method that takes a number as an argument and prints "Valid" if the number is between 1 and 10 (inclusive) and "Invalid" otherwise.
+# def num_between(num)
+#   if num <= 10 && num >= 1
+#     'valid'
+#   else
+#     "invalid"
+#   end
+# end
+
+# p num_between(2)
+
+# Create a method that takes in a string and determines if the string is a palindrome.
+
+# def palindrome(string)
+#   if string == string.reverse
+#     "#{string} is a palindrome"
+#   else
+#     "#{string} is not a palindrome"
+#   end
+# end
+
+# p palindrome('mom')
+# p palindrome('seth')
+# p palindrome('racecar')
+
+
+
+# ✂️ Challenge: Rock, Paper, Scissors
+# As the first user, I can see a prompt in the terminal to enter my name.
+# As the second user, I can see a prompt in the terminal to enter my name.
+# As the first user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+# As the second user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
+# As a user, I can see a message in the terminal depicting which user won the round.
+# As a user, I can see a message in the terminal noting if there was a tie.
+
+
+
+
+# ✅ Challenge: Password Checker
+# As a developer, you have been tasked with creating the user registration for a secure web site. To test your code, enter the user ID and password in the terminal to find out if they are acceptable. The user ID and password must adhere to the to following criteria:
+
+# User ID and password cannot be the same.
+# User ID and password must be at least six characters long.
+# Password must contain at least one of: !#$
+# User ID cannot contain the following characters: !#$ or spaces
+# Password cannot be the word "password".
+# User password must contain at least one number.
