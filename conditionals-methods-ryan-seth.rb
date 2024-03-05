740e001b1ab4ee8a8ab196ffb3a6e3cc0f48b8bf
sethcrist <sethcrist50@gmail.com>
2024-03-04 16:50:25 -0800

    finished

--- conditionals-methods-ryan-seth.rb
+++ conditionals-methods-ryan-seth.rb
@@ -48,10 +48,10 @@
 # ✂️ Challenge: Rock, Paper, Scissors
 # As the first user, I can see a prompt in the terminal to enter my name. ✅
 # As the second user, I can see a prompt in the terminal to enter my name. ✅
-# As the first user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
-# As the second user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors".
-# As a user, I can see a message in the terminal depicting which user won the round.
-# As a user, I can see a message in the terminal noting if there was a tie.
+# As the first user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors". ✅
+# As the second user, I can see a prompt in the terminal asking me to type either "rock", "paper", or "scissors". ✅
+# As a user, I can see a message in the terminal depicting which user won the round. ✅
+# As a user, I can see a message in the terminal noting if there was a tie. ✅
 
 puts 'enter player one user_name'
 user_one = gets.chomp
@@ -81,11 +81,12 @@ end
 p game_outcome(user_one, user_one_selection, user_two, user_two_selection)
 
 # ✅ Challenge: Password Checker
-# As a developer, you have been tasked with creating the user registration for a secure web site. To test your code, enter the user ID and password in the terminal to find out if they are acceptable. The user ID and password must adhere to the to following criteria:
+# As a developer, you have been tasked with creating the user registration for a secure web site. To test your code, enter the user ID and password in the terminal to find out if they are acceptable. 
+# The user ID and password must adhere to the to following criteria:
 
-# User ID and password cannot be the same.
+# User ID and password cannot be the same. 
 # User ID and password must be at least six characters long.
-# Password must contain at least one of: !#$
+# Password must contain at least one of: !#$ 
 # User ID cannot contain the following characters: !#$ or spaces
 # Password cannot be the word "password".
 # User password must contain at least one number.
