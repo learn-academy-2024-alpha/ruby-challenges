ac215db8f573cb87c1d75b5938afc535adb04c5c
mark19242 <mark19242@gmail.com>
2024-03-06 16:41:17 -0800

    finished

--- classes-amir-mark.rb
+++ classes-amir-mark.rb
@@ -45,27 +45,18 @@ end
   dishes = Task.new("Wash dishes")
   vaccum = Task.new("Hide the dog")
  
-  p "#{laundry.get_title} #{laundry.get_status}"
-  p "#{vaccum.get_title} #{vaccum.get_status}" 
-  p "#{dishes.get_title} #{dishes.get_status}"  
-  p "the status of the laundry is #{laundry.get_status}"
-  p "the status of the vaccum is #{vaccum.get_status}" 
-  p "the status of the dishes is #{dishes.get_status}" 
+  # p "#{laundry.get_title} #{laundry.get_status}"
+  # p "#{vaccum.get_title} #{vaccum.get_status}" 
+  # p "#{dishes.get_title} #{dishes.get_status}"  
+  # p "the status of the laundry is #{laundry.get_status}"
+  # p"the status of the vaccum is #{vaccum.get_status}" 
+  # p"the status of the dishes is #{dishes.get_status}" 
 # As a developer, I can see the title of each instance of class Task.
-
-
- 
-
 # As a developer, I can initialize each instance of class Task with a status that has a default value of 'incomplete'.
-
-
-
 # As a developer, I can update the status of each instance of class Task when the task has been completed.
 
 
-
 # For the following ColorPalette challenge use initialize and attr_accessor methods in your class.
-
 # As a developer, I can create a class called ColorPalette.
 # As a developer, I can create three instances (objects) of class ColorPalette.
 # e.g green = ColorPalette.new
@@ -74,3 +65,23 @@ end
 # As a developer, I can print the value of each individual color.
 # As a developer, I can create a method called all_colors that when called will print a sentence telling me the three colors of a given palette.
 # As a developer, I can change one or more colors of a given palette.
+
+class ColorPalette
+  attr_accessor :color1, :color2, :color3
+  def initialize(color1, color2, color3)
+    @color1 = color1
+    @color2 = color2
+    @color3 = color3
+  end
+  def all_colors 
+    "The three colors of the palate are: #{@color1}, #{@color2}, #{@color3}"
+  end
+end
+
+green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
+blue = ColorPalette.new('Azura', 'Sky', 'Royal')
+red = ColorPalette.new('Burgundy', 'HotRod', 'Rose')
+
+green.color2 = "Forest"
+p green.color2
+p green
